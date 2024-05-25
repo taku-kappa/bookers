3b38bf5d62af31754a409a82ce60f87f4b041135
taku-kappa <168840294+taku-kappa@users.noreply.github.com>
2024-05-25 08:05:56 +0000

    [add] 新規登録機能作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,14 @@ class BooksController < ApplicationController
   def index
   end
 
+  def create
+    @book = Book.new
+    if @book.save
+      redirect_to books_path(@book.id)
+    else render :index
+    end
+  end
+
   def show
   end
 
