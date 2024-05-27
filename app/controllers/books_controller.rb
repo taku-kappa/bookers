b12d9ffd3a4d6608e45ee285b90329304e146de1
taku-kappa <168840294+taku-kappa@users.noreply.github.com>
2024-05-27 14:25:17 +0000

    [add] フラッシュメッセージまで追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,41 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def create
-    @book = Book.new
-    if @book.save
-      redirect_to books_path(@book.id)
+    book = Book.new(book_params)
+    if book.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to book_path(book.id)
     else render :index
     end
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "Book was successfully updated"
+    redirect_to action: "show"
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to action: "index"
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
